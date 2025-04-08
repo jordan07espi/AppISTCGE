d19977359257758329ed6528b138fea31ec6420b
jordan07espi <jeespinosa5@espe.edu.ec>
2025-04-08 13:03:33 -0500

    Funcionalidad fundacion terminada

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-tab3',
@@ -8,6 +9,22 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+ // Define URLs externas para todos los apartados
+ private externalUrls: Record<'fundacion' | 'voluntario' | 'contactos' | 'web', string> = {
+  fundacion: 'https://cgefoundation.org/somos/',
+  voluntario: 'https://cgefoundation.org/ser-parte/',
+  contactos: 'https://cgefoundation.org/contacto/',
+  web: 'https://cgefoundation.org/', 
+  };
 
+  async goTo(pageIdentifier: 'fundacion' |'voluntario' |'contactos' | 'web') {
+    console.log('Ir a:', pageIdentifier);
+
+    // Verifica si el identificador tiene una URL asociada
+    if (this.externalUrls[pageIdentifier]) {
+      await Browser.open({ url: this.externalUrls[pageIdentifier] });
+    } else {
+      console.log('Identificador no reconocido o sin URL definida:', pageIdentifier);
+    }
+  }
 }
