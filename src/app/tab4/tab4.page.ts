8523e0371aa3957dd8ca3359acc2de1ec0769bc0
jordan07espi <jeespinosa5@espe.edu.ec>
2025-04-11 12:47:09 -0500

    Se añade Tutoriales a vista Recursos

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -10,12 +10,13 @@ import { Browser } from '@capacitor/browser';
 export class Tab4Page {
 
  // Define URLs externas para todos los apartados
- private externalUrls: Record<'radio' | 'biblia', string> = {
+ private externalUrls: Record<'radio' | 'biblia' | 'tutoriales', string> = {
   radio: 'https://zeno.fm/radio/rey-de-vida-online-rna0/',
   biblia: 'https://www.bible.com/es/bible/149/JHN.3.RVR1960',
+  tutoriales: 'https://web.istcge.edu.ec/tutoriales/', 
   };
 
-  async goTo(pageIdentifier: 'radio' |'biblia') {
+  async goTo(pageIdentifier: 'radio' |'biblia'| 'tutoriales') {
     console.log('Ir a:', pageIdentifier);
 
     // Verifica si el identificador tiene una URL asociada
